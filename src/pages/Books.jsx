5088b37116e5f162836ecef4ad092a819606b1c0
Reza Merzaie <ghrezam97@gmail.com>
2023-07-18 23:03:08 +0430

    Display books from store

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,24 +1,30 @@
-import books from '../data/BooksData';
+// import books from '../data/BooksData';
+import { useSelector } from 'react-redux';
 import Book from '../components/Book';
 import AddBook from '../components/AddBook';
 
-const Books = () => (
-  <div className="container">
-    <div className="row gx-3 my-4 ">
-      {books.map((book) => (
-        <div key={book.id} className="col col-12 mb-3 border-none">
-          <Book
-            genre={book.genre}
-            name={book.title}
-            author={book.author}
-            percentage={book.percentage}
-            chapter={book.chapter}
-          />
+function Books() {
+  const books = useSelector((store) => store.books);
+  return (
+    <>
+      <div className="container">
+        <div className="row gx-3 my-4 ">
+          {books.map((book) => (
+            <div key={book.id} className="col col-12 mb-3 border-none">
+              <Book
+                genre={book.genre}
+                name={book.title}
+                author={book.author}
+                percentage={25}
+                chapter="Chapter 1"
+              />
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-    <AddBook />
-  </div>
-);
+        <AddBook />
+      </div>
+    </>
+  );
+}
 
 export default Books;
