ed5de495303d5a14f5b017f15278688f3cd3f713
Reza Merzaie <ghrezam97@gmail.com>
2023-07-20 17:51:20 +0430

    Display books from API

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,23 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = [
+const response = await axios.get(
+  'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/iq87ZDBdLVe5avxJUeoP/books',
+);
+const { data } = response;
+
+const books = Object.keys(data).map((key) => {
+  const apiBooks = data[key][0];
+  return {
+    ...apiBooks,
+    item_id: parseInt(key, 10),
+  };
+});
+
+const initialState = books || [
   {
     item_id: 1,
-    title: 'The Great Gatsby',
-    author: 'John Smith',
-    category: 'Fiction',
-  },
-  {
-    item_id: 2,
-    title: 'Anna Karenina',
-    author: 'Leo Tolstoy',
-    category: 'Fiction',
-  },
-  {
-    item_id: 3,
-    title: 'The Selfish Gene',
-    author: 'Richard Dawkins',
-    category: 'Nonfiction',
+    title: 'There is no book',
+    author: '',
+    category: '',
   },
 ];
 
